d8ea318e1014edf54f0666c23903d84c49f7ec78
vnpq <phgquynh2208@gmail.com>
2026-05-28 20:42:33 +0700

    edit folder design

---
+++ src/self_rag_verifier/__init__.py
@@ -0,0 +1,12 @@
+"""
+Data loading and preprocessing module.
+
+Provides the ASQA dataset loader and shared data models
+(TrainExample, RetrieverExample).
+"""
+
+from .verifier_system import VerifierSystem
+
+__all__ = [
+    "VerifierSystem",
+]
